2c9fa58541d324fa781f506dd1ac6301c50fd771
panbingxin <bingxin.25@intl.zju.edu.cn>
2026-03-11 09:50:27 +0100

    recommit infection part

--- Practical4/infection.py
+++ Practical4/infection.py
@@ -1,14 +1,12 @@
 #initial population
 #growth rate
 #the next day population
-#count wich day
+#count which day
 #terminal when population reaches 91
 a=5 #initial population
 b=1.4 #growth rate
-n=1 #count wich day
-while a<91: #terminal when population reaches 91
+n=1 #count which day
+while a<=91: #terminal when population reaches 91
     n +=1
     a=a*b #population of next day
-n=n+1
 print(n)
-n=11
