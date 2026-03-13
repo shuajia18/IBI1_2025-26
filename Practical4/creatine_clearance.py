8f33a97c1e317976933318651c78ab447700f606
panbingxin <bingxin.25@intl.zju.edu.cn>
2026-03-13 14:52:44 +0100

    revise

--- Practical4/creatine_clearance.py
+++ Practical4/creatine_clearance.py
@@ -2,13 +2,26 @@
 #know the weight
 #know whether female or not
 #creatine concentration
+#check whether variables in normal range
 #how to make calculate
 #give the result
 
 a=1 #konw the age 
 b=49 #know the weight
 c=40 #creatine concentration
-d=(140-a)*b*0.85/(72*c) #how to make calculate
-print(c) #give the result
-#answer is 40
+d=0
+gender="female"
+if a>100:
+    d=1
+if b<=20 or b>=80:
+    d=1
+if c<=0 or c>=100:
+    d=1
+if d==0:
+    if gender=="female":
+        e=(140-a)*b*0.85/(72*c) #how to make calculate
+    else:
+        e=(140-a)*b/(72*c) #how to make calculate
+print(e) #give the result
+#answer is 2.010190972222222
 
--- Practical4/infection.py
+++ Practical4/infection.py
@@ -6,7 +6,7 @@
 a=5 #initial population
 b=1.4 #growth rate
 n=1 #count which day
-while a<=91: #terminal when population reaches 91
+while a<91: #terminal when population reaches 91
     n +=1
     a=a*b #population of next day
 print(n)
