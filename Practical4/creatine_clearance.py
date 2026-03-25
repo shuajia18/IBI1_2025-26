bdaf4dbf0c109cd3e6f1f3d3a3445b64a81f270b
panbingxin <bingxin.25@intl.zju.edu.cn>
2026-03-25 09:40:48 +0100

    modify creatine_clearance.py part

--- Practical4/creatine_clearance.py
+++ Practical4/creatine_clearance.py
@@ -13,15 +13,21 @@ d=0
 gender="female"
 if a>100:
     d=1
+    print('age variable needs to be corrected')#tell age variable needs corrected
 if b<=20 or b>=80:
     d=1
+    print('weight variable needs to be corrected')#tell weight variable needs corrected
 if c<=0 or c>=100:
     d=1
+    print('creatine concentration variable needs to be corrected')#tell creatine concentration needs corrected
 if d==0:
     if gender=="female":
         e=(140-a)*b*0.85/(72*c) #how to make calculate
     else:
         e=(140-a)*b/(72*c) #how to make calculate
-print(e) #give the result
-#answer is 2.010190972222222
+        print(e) #give the result
+        #answer is 2.010190972222222
+else:
+    print('the variables do not meet requirement')
+
 
