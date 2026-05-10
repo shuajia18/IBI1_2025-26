64ddca40dfaa537864db322b34201ec1721c12c8
panbingxin <bingxin.25@intl.zju.edu.cn>
2026-05-10 10:48:30 +0100

    add a print statement for the clearance rate if the input is female or male

--- Practical4/creatine_clearance.py
+++ Practical4/creatine_clearance.py
@@ -7,10 +7,11 @@
 #give the result
 
 a=1 #konw the age 
-b=49 #know the weight
+b=70 #know the weight
 c=40 #creatine concentration
 d=0
 gender="female"
+
 if a>100:
     d=1
     print('age variable needs to be corrected')#tell age variable needs corrected
@@ -20,13 +21,14 @@ if b<=20 or b>=80:
 if c<=0 or c>=100:
     d=1
     print('creatine concentration variable needs to be corrected')#tell creatine concentration needs corrected
+
 if d==0:
     if gender=="female":
         e=(140-a)*b*0.85/(72*c) #how to make calculate
-    else:
+    if gender=='male':
         e=(140-a)*b/(72*c) #how to make calculate
-        print(e) #give the result
-        #answer is 2.010190972222222
+    print(f"The creatinine clearance rate for {gender} is: {e}")  #give the result
+
 else:
     print('the variables do not meet requirement')
 
