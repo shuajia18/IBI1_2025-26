e358a7667ce0ed00901ffce68e23823bcfb9f55f
panbingxin <bingxin.25@intl.zju.edu.cn>
2026-05-10 10:37:23 +0100

    add code to  print the number of infected individuals every day in infection.py

--- Practical4/infection.py
+++ Practical4/infection.py
@@ -6,8 +6,10 @@
 a=5 #initial population
 b=1.4 #growth rate
 n=1 #count which day
-while a<91: #terminal when population reaches 91
+while a< 91: #terminal when n reaches 91
+    print(f'the number of infected students for Day n is {int(a)}')
     n +=1
     a=a*b #population of next day
-print(n)
+    
+print(f'At Day {n}, all students are infected')
 #n=10
